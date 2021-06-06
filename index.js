e0cf9bc181e0dd643bd726b8d007491db36b1d1e
jenny-jny <jenny.yu.0414@gmail.com>
2021-06-06 12:00:08 -0500

    Create express GET routes and add comments

--- index.js
+++ index.js
@@ -33,3 +33,17 @@ let topMovies = [
     title: 'Gone with the Wind'
   }
 ];
+
+//GET requests
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+//listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
