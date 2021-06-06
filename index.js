9c84da9476c1ec57ac5570134f370a844204f962
jenny-jny <jenny.yu.0414@gmail.com>
2021-06-06 11:53:22 -0500

    Add object array of topMovies

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Casablanca'
+  },
+  {
+    title: 'Up'
+  },
+  {
+    title: 'Slumdog Millionaire'
+  },
+  {
+    title: 'Gaslight'
+  },
+  {
+    title: 'Titanic'
+  },
+  {
+    title: 'The Matrix'
+  },
+  {
+    title: 'The Great Gatsby'
+  },
+  {
+    title: 'Once Upon A Time in Hollywood'
+  },
+  {
+    title: 'Forrest Gump'
+  },
+  {
+    title: 'Gone with the Wind'
+  }
+];
