864fa797ae529794a2b713fcf27e6cbd68be4a29
jenny-jny <jenny.yu.0414@gmail.com>
2021-06-23 19:19:08 -0500

    Configure passport strategies - LocalStrategy and JWTStrategy

---
+++ passport.js
@@ -0,0 +1,39 @@
+const Models = require('./models.js'),
+Users = Models.User;
+
+const passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy;
+
+const passportJWT = require('passport-jwt');
+JWTStrategy = passportJWT.Strategy,
+ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password'
+}, (username, password, callback) => {
+  console.log(username + ' ' + password);
+  Users.findOne({Username: username}, (error, user) => {
+    if(error){
+      console.log(error);
+      return callback(error);
+    }
+    if(!user){
+      console.log('Incorrect username');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
+    console.log('Finished');
+    return callback(null, user);
+  })
+}));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id).then((user) => {
+    return callback(null, user);
+  }).catch((error) => {
+    return callback(error);
+  });
+}));
