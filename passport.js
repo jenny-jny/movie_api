6822f2db603c53a2b85b94dd77fc6bc0bbd313c8
jenny-jny <jenny.yu.0414@gmail.com>
2021-06-24 23:06:12 -0500

    Add comments

--- passport.js
+++ passport.js
@@ -8,32 +8,32 @@ const passportJWT = require('passport-jwt');
 JWTStrategy = passportJWT.Strategy,
 ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password'
-}, (username, password, callback) => {
+passport.use(new LocalStrategy({ //before asking Passport to authenticate a request, the strategy (or strategies) used by an application must be configured. Strategies, and their configuration, are supplied via the use() function.
+  usernameField: 'Username', //parses credentials contained in the request
+  passwordField: 'Password' //parses credentials contained in the request
+}, (username, password, callback) => { //then invokes verify (authentication) callback with credentials as arguments
   console.log(username + ' ' + password);
   Users.findOne({Username: username}, (error, user) => {
     if(error){
       console.log(error);
-      return callback(error);
+      return callback(error); //server exception (error)
     }
     if(!user){
       console.log('Incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      return callback(null, false, {message: 'Incorrect username or password.'}); //error = null, user = false
     }
     console.log('Finished');
-    return callback(null, user);
+    return callback(null, user); //error = null
   })
 }));
 
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
-}, (jwtPayload, callback) => {
-  return Users.findById(jwtPayload._id).then((user) => {
-    return callback(null, user);
+passport.use(new JWTStrategy({ //before asking Passport to authenticate a request, the strategy (or strategies) used by an application must be configured. Strategies, and their configuration, are supplied via the use() function.
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(), //parses credentials contained in the request
+  secretOrKey: 'your_jwt_secret' //parses credentials contained in the request
+}, (jwtPayload, callback) => { //then invokes verify (authentication) callback with credentials as arguments
+  return Users.findById(jwtPayload._id).then((user) => { //when a promise is resolved, it can optionally return a value. This piece of data is assigned to the parameters of the callback function in .then(). A promise is a returned object to which you attach callbacks, instead of passing callbacks into a function. 
+    return callback(null, user); //error = null
   }).catch((error) => {
-    return callback(error);
+    return callback(error); //server exception (error)
   });
 }));
