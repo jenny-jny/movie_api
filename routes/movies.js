bfba3161d2cd212790ee82e02518df865e8be003
jenny-jny <jenny.yu.0414@gmail.com>
2021-06-29 22:10:10 -0500

    Add movies endpoints, passport middleware, and controller functions

---
+++ routes/movies.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+router = express.Router(),
+passport = require('passport');
+require('../passport');
+const moviesController = require('../controllers/movies');
+
+router.get('/movies', passport.authenticate('jwt', {session: false}), moviesController.getMovies);
+router.get('/movies/:Title', passport.authenticate('jwt', {session: false}), moviesController.getMovieTitle);
+router.get('/movies/:Title/genre', passport.authenticate('jwt', {session: false}), moviesController.getMovieGenre);
+router.get('/movies/:Title/director', passport.authenticate('jwt', {session: false}), moviesController.getMovieDirector);
+
+module.exports = router;
