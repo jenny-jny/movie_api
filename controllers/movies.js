6be4b48e4ae24848e3fcd6ad934faf822e913e0e
jenny-jny <jenny.yu.0414@gmail.com>
2021-06-29 22:11:29 -0500

    Add movies controller functions' logic

---
+++ controllers/movies.js
@@ -0,0 +1,49 @@
+Models = require('../models');
+const Movies = Models.Movie;
+
+//authentication
+// const authenticate = (passport) => {
+//   passport.authenticate('jwt', {session: false});
+// },
+
+//GET a list of ALL movies
+getMovies = (req, res) => {
+  Movies.find().then((movies) => {
+    res.status(201).json(movies);
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+},
+
+//get the data of a single movie by title
+getMovieTitle = (req, res) => {
+  Movies.findOne({Title: req.params.Title}).then((movie) => {
+    res.json(movie);
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+},
+
+//get data (descrition) about a genre by movie title 
+getMovieGenre = (req, res) => {
+  Movies.findOne({Title: req.params.Title}).then((movie) => {
+    res.json(movie.Genre);
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  })
+},
+
+//get data about a director by movie title 
+getMovieDirector = (req, res) => {
+  Movies.findOne({Title: req.params.Title}).then((movie) => {
+    res.json(movie.Director);
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  })
+}
+
+module.exports = {getMovies, getMovieTitle, getMovieGenre, getMovieDirector};
