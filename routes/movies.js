22ef03dd4b2b78006060242dbd481a813a721823
jenny-jny <jenny.yu.0414@gmail.com>
2021-07-26 14:36:54 -0500

    Re-add passport authentication middleware from movies/ endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ router = express.Router(),
 moviesController = require('../controllers/movies');
 
 //authenticate() because needs to be immediately called, unlike getMovies, which is an asynch function
-router.get('/', moviesController.getMovies);
+router.get('/', moviesController.authenticate(), moviesController.getMovies);
 router.get('/:Title', moviesController.authenticate(), moviesController.getMovieTitle);
 router.get('/:Title/genre', moviesController.authenticate(), moviesController.getMovieGenre);
 router.get('/:Title/director', moviesController.authenticate(), moviesController.getMovieDirector);
