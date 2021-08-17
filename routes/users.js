d73c6ec891d93e33d31b466ab9d8f95c8b1d7801
jenny-jny <jenny.yu.0414@gmail.com>
2021-08-16 20:07:50 -0500

    Add get user information by username route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ usersController = require('../controllers/users');
 
 //userInputValidate() and authenticate() because needs to be immediately called, unlike getMovies, which is an asynch function
 router.post('/register', usersController.userInputValidate(), usersController.addUser);
+router.get('/users/:Username', usersController.authenticate(), usersController.userInputValidate(), usersController.getUser);
 router.put('/users/:Username', usersController.authenticate(), usersController.userInputValidate(), usersController.updateUser);
 router.post('/users/:Username/favorites/:MovieID', usersController.authenticate(), usersController.addUserFavoriteMovie);
 router.delete('/users/:Username/favorites/:MovieID', usersController.authenticate(), usersController.removeUserFavoriteMovie);
